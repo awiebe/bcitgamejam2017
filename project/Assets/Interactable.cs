28c131cad5aa2c0580a04f9a27ddb3f7a19aa7c3
Pierce <pcarruth@sfu.ca>
2017-02-11 16:37:07 -0800

    Added Interactable

---
+++ project/Assets/Interactable.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public static ArrayList interactables = new ArrayList();
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+        interactables.Add(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnDestroy()
+    {
+        interactables.Remove(this);
+    }
+
+    public void OnInteract()
+    {
+        this.target.SendMessage("onInteract", this);
+    }
+
+    public Interactable GetClosest(Vector2 p)
+    {
+        if(Interactable.interactables.Count == 0)
+        {
+            return null;
+        }
+
+        Interactable closest = null;
+        closest = (Interactable)Interactable.interactables[0];
+        float closestDistance = Vector2.Distance(closest.transform.position, p);
+
+        foreach (Interactable i in Interactable.interactables) 
+        {
+            //If objects distance > distance, make it the new distance
+            Transform t = i.transform;
+
+            float currentDistance = Vector2.Distance(p, t.position);
+
+
+            if (currentDistance < closestDistance)
+            {
+                closestDistance = currentDistance;
+                closest = i;
+            }
+        }
+
+        return closest;
+    }
+}
---
+++ project/Assets/Scripts/GDC Member Scripts/HumphreyMove.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumphreyMove : MonoBehaviour
+{
+	Animator dino;
+	// Animation state machine for dino
+	public float maxSpeed;
+	// The maximum speed the dino can go
+	public float speedX;
+    public float speedY;
+	// The amount of speed
+	public float jumpForce;
+	// The force of each jump
+	public GameObject poop;
+
+	private float moveX;
+	private bool facingRight;
+
+    private float moveY;
+
+	private bool jump;
+	// TRUE if dino can jump. FALSE otherwise
+	private bool grounded;
+	// TRUE if dino is touching the ground. FALSE otherwise
+	RaycastHit2D hit;
+	//Ray2D ray -> what does this do?
+
+	// Use this for initialization
+	void Start ()
+	{
+		dino = gameObject.GetComponent<Animator>();  //initialize it as the state machine attached to the dino object so we can reference it in this code
+	}
+
+	// Gound detection control -- Touched the ground
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "ground")
+			grounded = true;
+	}
+
+	// Gound detection control -- Left the ground
+	void OnCollisionExit2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "ground")
+			grounded = false;
+	}
+
+	// Flip the character when switching directions from left to right
+	void Flip ()
+	{
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		facingRight = !facingRight;
+	}
+
+	void FixedUpdate ()
+	{
+		moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+		// To the right, to the right
+		if (moveX > 0 && !facingRight)
+		{
+			Flip();
+		}
+
+		// To the left, to the left
+		else
+		if (moveX < 0 && facingRight)
+		{
+			Flip();
+		}
+
+        dino.SetFloat("move", moveX);
+		Vector2 movement = new Vector2(moveX * speedX, moveY * speedY);
+		GetComponent<Rigidbody2D>().AddForce(movement);
+        
+
+		// Limit max horizontal speed
+		if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		// When touching the ground and player inputs 'Jump'
+		if (Input.GetButton("Jump") && grounded)
+		{
+			jump = true;
+			GameObject.Instantiate(this.poop, this.transform.position, this.transform.rotation);
+		}
+
+		// Jumping sequence
+		if (jump)
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+	}
+}

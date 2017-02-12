61034d366b4a2f25356cb98a3910a2b781c05b5e
Pierce <pcarruth@sfu.ca>
2017-02-12 02:47:28 -0800

    TV toggles on and off~

---
+++ project/Assets/TvController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TvController : MonoBehaviour {
+    public Animator animator;
+    public SpriteRenderer renderer;
+    public Sprite offSprite;
+    public bool isOn;
+    void OnInteract(MonoBehaviour sender)
+    {
+        isOn = !isOn;
+        if (isOn)
+        {
+            animator.enabled = true;
+        }
+        else
+        {
+            animator.enabled = false;
+            renderer.sprite = offSprite;
+        }
+    }
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
